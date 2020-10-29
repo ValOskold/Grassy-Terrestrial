de460eff396133f58613bb34d30d2dfe1e7e4ef4
ValOskold <ValOskold@protonmail.com>
2020-10-29 00:29:44 -0400

    Registration
    Preforms a check to see if the username or email has been taken and returns a message.

---
+++ registrationPage.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+	<body>
+		<form action="processing-RegistrationPage.php" method="POST">
+	firstName: <input type="text" name="userName" />
+	email: <input type="text" name="emailAddress" />
+	passPhrase: <input type="text" name="password"/>
+	<input type="submit" />
+</form>
+	</body>
+</html>
