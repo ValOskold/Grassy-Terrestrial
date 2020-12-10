64e9365d1257fe97aa9b39eac2b7c4b8a714fda3
fionahowings <71687694+howings@users.noreply.github.com>
2020-12-10 16:05:44 -0500

    quick solution for the logout redirect
    just a quick solution for now with a window popup to confirm that user has been logout before redirection back to the landing page.

--- logout.php
+++ logout.php
@@ -3,5 +3,11 @@
 	session_start();
  	session_destroy();
 	 //display logout message, return to landingpage
-	header("Location: index.html");
+	echo("
+	<script>
+	alert('Logout successful.');
+	window.location.replace('index.html');
+	</script>
+	");
+	//header("Location: index.html");
 ?>
